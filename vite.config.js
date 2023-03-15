f5b05ee548267e41e118daeeb4fd74cd916d349d
rizkiyusra <12050113426@students.uin-suska.ac.id>
2023-03-15 19:45:27 +0700

    menghapus folder .vscode dari repo

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,30 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  publicPath: '/',
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Vue App'
+      args[0].template = './public/index.html'
+      args[0].filename = 'index.html'
+      args[0].chunksSortMode = 'manual'
+      args[0].chunks = ['chunk-vendors', 'chunk-common', 'index']
+      args[0].excludeChunks = ['404']
+      return args
+    })
+    config.plugin('copy').tap(args => {
+      args[0][0].ignore.push('404.html')
+      return args
+    })
+  },
+  configureWebpack: {
+    entry: {
+      app: './src/main.js',
+      404: './src/views/Error404.vue'
+    },
+    output: {
+      filename: '[name].[hash].js'
+    }
   }
 })
