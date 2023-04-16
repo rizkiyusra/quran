81cebc05572ceebe0e4c3e61cb71bb86c45e4223
rizkiyusra <12050113426@students.uin-suska.ac.id>
2023-04-16 12:36:31 +0700

    update repo

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
@@ -11,19 +9,14 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  publicPath: '/',
   chainWebpack: config => {
     config.plugin('html').tap(args => {
       args[0].title = 'Vue App'
       args[0].template = './public/index.html'
-      args[0].filename = 'index.html'
+      args[0].filename = '404.html'
       args[0].chunksSortMode = 'manual'
-      args[0].chunks = ['chunk-vendors', 'chunk-common', 'index']
-      args[0].excludeChunks = ['404']
-      return args
-    })
-    config.plugin('copy').tap(args => {
-      args[0][0].ignore.push('404.html')
+      args[0].chunks = ['chunk-vendors', 'chunk-common', '404']
+      args[0].excludeChunks = ['index']
       return args
     })
   },
@@ -33,7 +26,7 @@ export default defineConfig({
       404: './src/views/Error404.vue'
     },
     output: {
-      filename: '[name].[hash].js'
+      filename: 'js/[name].[hash].js'
     }
   }
 })
